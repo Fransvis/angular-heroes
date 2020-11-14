2cbb9601f51083508f3596d1d2b250df514f6da3
Fransvis <francois.haf@gmail.com>
2020-11-14 21:31:19 +0200

    Fransvis: refactored code and added styling for each component

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -5,15 +5,14 @@ import { HeroService } from '../hero.service';
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.scss']
+  styleUrls: [ './dashboard.component.scss' ]
 })
 export class DashboardComponent implements OnInit {
-
   heroes: Hero[] = [];
 
   constructor(private heroService: HeroService) { }
 
-  ngOnInit()  {
+  ngOnInit() {
     this.getHeroes();
   }
 
@@ -21,5 +20,4 @@ export class DashboardComponent implements OnInit {
     this.heroService.getHeroes()
       .subscribe(heroes => this.heroes = heroes.slice(1, 5));
   }
-
 }
